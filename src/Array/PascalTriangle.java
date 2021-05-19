92f2270ba27b9abfe2edd9e9c8a4d079b89bd65c
pratik2550 <pratikpatle250@gmail.com>
2021-05-19 12:51:45 +0530

    Generate nth row of PascalTriangle

--- src/Array/PascalTriangle.java
+++ src/Array/PascalTriangle.java
@@ -1,4 +1,7 @@
 package Array;
+/*
+    Program to generate Pascal's Triangle and generate nth row of Pascal Triangle
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,15 +16,13 @@ public class PascalTriangle {
         while (t-->0) {
             int n = Integer.parseInt(br.readLine());
             List<List<Integer>> res = generatePascalTriangle(n);
-            for (int i=0; i<n; i++) {
-                for (int j=0; j<=i; j++) {
-                    System.out.print(res.get(i).get(j)+" ");
-                }
-                System.out.println();
-            }
+            printTriangle(res);
+            List<Long> getRow = getRow(n);
+            printRow(getRow);
         }
     }
 
+    //generate Pascal triangle
     private static List<List<Integer>> generatePascalTriangle(int n) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> pre = new ArrayList<>();
@@ -38,6 +39,36 @@ public class PascalTriangle {
             res.add(row);
         }
         return res;
+    }
+
+    //print Pascal triangle
+    private static void printTriangle(List<List<Integer>> res) {
+        for (int i=0; i<res.size(); i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print(res.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    //generate nth row of Pascal triangle
+    private static List<Long> getRow(int n) {
+        List<Long> res = new ArrayList<>();
+        res.add(1L);
+        long temp = 1;
+        for(int i=1,up=n,down=1;i<=n;i++,up--, down++){
+            temp=temp*up/down;
+            res.add(temp);
+        }
+        return res;
+    }
+
+    //print nth row of Pascal Triangle
+    private static void printRow(List<Long> getRow) {
+        for (int i=0; i<getRow.size(); i++) {
+            System.out.print(getRow.get(i)+" ");
+        }
+        System.out.println();
     }
 
 }
