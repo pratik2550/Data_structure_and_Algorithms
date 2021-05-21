c75d10f38810b8781e9162ce0b8465adf8abaaaa
pratik2550 <pratikpatle250@gmail.com>
2021-05-22 00:42:21 +0530

    Anti-clockwise rotate Matrix.

--- src/Array/RotateMatrix.java
+++ src/Array/RotateMatrix.java
@@ -32,6 +32,7 @@ public class RotateMatrix {
                 matrix[j][i] = temp;
             }
         }
+        //clockwise rotation
         for (int i=0; i<matrix.length; i++) {
             for (int j=0; j<matrix.length/2; j++) {
                 int temp = matrix[i][j];
@@ -39,6 +40,14 @@ public class RotateMatrix {
                 matrix[i][n-j-1] = temp;
             }
         }
+//        Anti-clockwise rotation
+//        for (int i=0; i<matrix.length/2; i++) {
+//            for (int j=0; j<matrix.length; j++) {
+//                int temp = matrix[i][j];
+//                matrix[i][j] = matrix[n-i-1][j];
+//                matrix[n-i-1][j] = temp;
+//            }
+//        }
         printMatrix(matrix);
     }
 
