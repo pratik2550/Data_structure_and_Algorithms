4f581ecf136a620b045b28941d6ef4f06d9308a7
pratik2550 <pratikpatle250@gmail.com>
2021-05-27 23:56:08 +0530

    Four Sum.

--- src/Hashing/FourSum.java
+++ src/Hashing/FourSum.java
@@ -27,17 +27,16 @@ public class FourSum {
             for (int j = i + 1; j < arr.length; j++) {
                 for (int k = j + 1; k < arr.length; k++) {
                     int tar = target - arr[i] - arr[j] - arr[k];
-                    int l = k + 1, r = arr.length-1;
+                    int l = k + 1, r = arr.length - 1;
                     int mid = binarySearch(arr, l, r, tar);
-                    if (mid == Integer.MIN_VALUE) {
-                        break;
+                    if (mid != Integer.MIN_VALUE) {
+                        List<Integer> quad = new ArrayList<>();
+                        quad.add(arr[i]);
+                        quad.add(arr[j]);
+                        quad.add(arr[k]);
+                        quad.add(arr[mid]);
+                        pair.add(quad);
                     }
-                     List<Integer> quad = new ArrayList<>();
-                    quad.add(arr[i]);
-                    quad.add(arr[j]);
-                    quad.add(arr[k]);
-                    quad.add(arr[mid]);
-                    pair.add(quad);
 //                    while (k+1<arr.length && arr[k+1]==quad.get(2)) ++k;
                 }
 //                while (j+1<arr.length && arr[j+1] == arr[j]) ++j;
@@ -49,7 +48,7 @@ public class FourSum {
 
     private static int binarySearch(int[] arr, int l, int r, int tar) {
         if (l <= r) {
-            int mid = l+(r - l) / 2;
+            int mid = l + (r - l) / 2;
             if (arr[mid] == tar) {
                 return mid;
             } else if (arr[mid] > tar) {
@@ -63,7 +62,7 @@ public class FourSum {
 
     private static List<List<Integer>> countSun2(int[] arr, int target) {
         List<List<Integer>> pair = new ArrayList<>();
-        if (arr==null || arr.length==0) {
+        if (arr == null || arr.length == 0) {
             return pair;
         }
         Arrays.sort(arr);
@@ -83,13 +82,13 @@ public class FourSum {
                         quad.add(arr[l]);
                         quad.add(arr[r]);
                         pair.add(quad);
-                        while (l<r && arr[l]==quad.get(2)) ++l;
-                        while (l<r && arr[r]==quad.get(3)) --r;
+                        while (l < r && arr[l] == quad.get(2)) ++l;
+                        while (l < r && arr[r] == quad.get(3)) --r;
                     }
                 }
-                while (j+1<arr.length && arr[j+1] == arr[j]) ++j;
+                while (j + 1 < arr.length && arr[j + 1] == arr[j]) ++j;
             }
-            while (i+1<arr.length && arr[i+1] == arr[i]) ++i;
+            while (i + 1 < arr.length && arr[i + 1] == arr[i]) ++i;
         }
         return pair;
     }
