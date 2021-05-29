44a5451715c747e2a1ed5c96685d61aaf6f2acce
pratik2550 <pratikpatle250@gmail.com>
2021-05-29 16:55:09 +0530

    Count number of sub array with given XOR.

---
+++ src/Hashing/SubarrayWithGivenXOR.java
@@ -0,0 +1,55 @@
+package Hashing;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class SubarrayWithGivenXOR {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputLine[] = br.readLine().trim().split(" ");
+        int arr[] = new int[inputLine.length];
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(inputLine[i]);
+        }
+        int target = Integer.parseInt(br.readLine());
+
+        System.out.println(countGivenXOR1(arr, target));
+        System.out.println(countSun2(arr, target));
+    }
+
+    private static int countGivenXOR1(int[] arr, int target) {
+        int count = 0;
+        for (int i=0; i<arr.length; i++) {
+            int xor = 0;
+            for (int j=i; j<arr.length; j++) {
+                xor = arr[j]^xor;
+                if (xor==target) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static int countSun2(int[] arr, int target) {
+        int count = 0;
+        int xor = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i=0; i<arr.length; i++) {
+            xor = xor^arr[i];
+            if (map.get(xor^target)!=null) {
+                count += map.get(xor^target);
+            }
+            if (xor==target) {
+                count++;
+            }
+            if (map.get(xor)!=null) {
+                map.put(xor, map.get(xor)+1);
+            } else {
+                map.put(xor, 1);
+            }
+        }
+        return count;
+    }
+}
