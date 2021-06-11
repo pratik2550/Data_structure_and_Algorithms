aa1fb1a6dfa9d17aae36562f4d520f5b0ca00aa7
pratik2550 <pratikpatle250@gmail.com>
2021-06-11 23:54:59 +0530

    Remove duplicate from sorted array.

---
+++ src/Array/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,34 @@
+package Array;
+/*
+    Program to remove duplicate from sorted array.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RemoveDuplicateFromSortedArray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String inputLine[] = br.readLine().trim().split(" ");
+            int arr[] = new int[inputLine.length];
+            for (int i=0; i<arr.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            System.out.println(removeDuplicate(arr));
+
+
+    }
+
+    private static int removeDuplicate(int[] arr) {
+        int j=0;
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i]!=arr[i+1]) {
+                j++;
+                arr[j] = arr[i+1];
+            }
+        }
+        return ++j;
+    }
+}
