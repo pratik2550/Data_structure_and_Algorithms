c44edd1e074cba6ea95914605f8dcc55cf44f370
pratik2550 <pratikpatle250@gmail.com>
2022-01-18 01:22:14 +0530

    Kth largest sum contiguous sub-array

--- src/Heap/KthLargestSumContiguousSubarray.java
+++ src/Heap/KthLargestSumContiguousSubarray.java
@@ -1,46 +1,45 @@
 package Heap;
 
+import java.io.*;
 import java.util.PriorityQueue;
 
 public class KthLargestSumContiguousSubarray {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        int arr[] = new int[n];
+//        for(int i=0; i<n; i++) {
+//            arr[i] = Integer.parseInt(br.readLine());
+//        }
+//        int k = Integer.parseInt(br.readLine());
+
         int a[] = new int[]{10, -10, 20, -40};
         int n = a.length;
         int k = 6;
 
-//        System.out.println(
-                kthLargestSum(a, n, k);
-//        );
+        System.out.println(kthLargestSum(a, n, k));
     }
 
-    static int kthLargestSum(int arr[], int n, int k) {
-        int sum[] = new int[n + 1];
+    private static int kthLargestSum(int[] arr, int n, int k) {
+        int sum[] = new int[n+1];
         sum[0] = 0;
         sum[1] = arr[0];
-        System.out.print(sum[0]+" ");
-        System.out.print(sum[1]+" ");
-        for (int i = 2; i <= n; i++) {
-            sum[i] = sum[i - 1] + arr[i - 1];
-            System.out.print(sum[i]+" ");
+        for(int i=2; i<=n; i++) {
+            sum[i] = sum[i-1]+arr[i-1];
         }
-        System.out.println();
-        PriorityQueue<Integer> Q = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i=1; i<=n; i++) {
+            for(int j=i; j<=n; j++) {
+                int x = sum[j]-sum[i-1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
-                int x = sum[j] - sum[i - 1];
-                System.out.print(x+" ");
-                if (Q.size() < k)
-                    Q.add(x);
-
-                else {
-                    if (Q.peek() < x) {
-                        Q.poll();
-                        Q.add(x);
-                    }
+                if(k > queue.size()) {
+                    queue.add(x);
+                } else if(queue.peek()<x){
+                    queue.remove();
+                    queue.add(x);
                 }
             }
         }
-        return Q.poll();
+        return queue.poll();
     }
 }
