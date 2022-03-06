7dc3d04b4b150fb18bcccf136282ef90429d4330
pratik2550 <pratikpatle250@gmail.com>
2022-03-07 01:18:46 +0530

    Mirror of a binary tree

--- src/Tree/MirrorOfTree.java
+++ src/Tree/MirrorOfTree.java
@@ -1,5 +1,7 @@
 package Tree;
 
+import sun.reflect.generics.tree.Tree;
+
 public class MirrorOfTree {
 
     static TreeNode root;
@@ -13,6 +15,22 @@ public class MirrorOfTree {
         treeLevel.root.left.right = new TreeNode(4);
 
         treeLevel.mirrorInorder(root);
+        System.out.println();
+        TreeNode mirror1 = mirrorify1(root);
+        printTree(mirror1);
+        System.out.println();
+        TreeNode mirror2 = mirrorify2(root);
+        printTree(root);
+
+    }
+
+    private static void printTree(TreeNode root) {
+        if (root == null)
+            return;
+
+        printTree(root.left);
+        System.out.print(root.data + " ");
+        printTree(root.right);
     }
 
     void mirrorInorder(TreeNode node)
@@ -25,5 +43,33 @@ public class MirrorOfTree {
         System.out.print(node.data + " ");
 
         mirrorInorder(node.left);
+    }
+
+    static TreeNode mirrorify1(TreeNode root)
+    {
+        if (root == null)
+        {
+            return null;
+
+        }
+
+        TreeNode mirror = new TreeNode(root.data);
+        mirror.right = mirrorify1(root.left);
+        mirror.left = mirrorify1(root.right);
+        return mirror;
+    }
+
+    public static TreeNode mirrorify2(TreeNode root)
+    {
+        if (root == null)
+            return null;
+
+        TreeNode left = mirrorify2(root.left);
+        TreeNode right = mirrorify2(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
     }
 }
